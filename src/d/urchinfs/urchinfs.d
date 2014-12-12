41824b662a619822d716842b3d69456e0f5eb8ea
Kellen Dye <kellen@cretin.net>
2014-12-12 11:29:49 +0100

    Partial

--- src/d/urchinfs/urchinfs.d
+++ src/d/urchinfs/urchinfs.d
@@ -21,57 +21,96 @@ class UrchinFSResult {
 }
 
 class UrchinFS : Operations {
-    static const string FACET_PREFIX = "+";
+    // FIXME certain characters are ignored for collation in UTF-8 locales LC_COLLATION
+    static const string FACET_PREFIX = "-";
     static const string OR = FACET_PREFIX ~ "OR";
     enum parsed { KEY, VAL, OR, NONE, DIR }
 
     // { metadata_key -> { metadata_value -> { display_name -> bool } }}
     // the last nested map is a hack due to no set type in dlang
 
-    bool[string][string][string] metadata_cache;
     UrchinFSEntry[] entries;
 
     this() {
         // TODO fetch actual data from disk
-        bool[string] year_dirs_48;
-        year_dirs_48["Easter Parade"] = true;
-        year_dirs_48["Force of Evil"] = true;
-
-        bool[string] year_dirs_12;
-        year_dirs_12["Skyfall"] = true;
-        year_dirs_12["Rust and Bone"] = true;
-
-        bool[string][string] years;
-        years["1948"] = year_dirs_48;
-        years["2012"] = year_dirs_12;
-
-        metadata_cache["year"] = years;
-
         UrchinFSEntry easter = new UrchinFSEntry();
         easter.display_name = "Easter Parade (1948)";
         string[][string] easter_md;
         easter_md["year"] = ["1948"];
+        easter_md["color"] = ["color"];
         easter.metadata = easter_md;
+        entries ~= easter;
+
+        UrchinFSEntry city = new UrchinFSEntry();
+        city.display_name = "The Naked City (1948)";
+        string[][string] city_md;
+        city_md["year"] = ["1948"];
+        city_md["color"] = ["black-and-white"];
+        city.metadata = city_md;
+        entries ~= city;
+
     }
 
-    string[] get_keys(UrchinFSEntry[] entries) {
-        string[] keys = [];
-        foreach(entry; entries) {
-            keys 
+    // prefix all strings with FACET_PREFIX
+    string[] facet_prefix(string[] keys) {
+        string[] ret;
+        foreach(key; keys) {
+            ret ~= (FACET_PREFIX ~ key);
         }
-        return keys;
+        return ret;
     }
 
+    // get all of the valid keys for the given entries
+    string[] get_keys(UrchinFSEntry[] entries) {
+        bool[string] keys;
+        foreach(entry; entries) {
+            foreach(string key, string[] v; entry.metadata) {
+                keys[key] = true;
+            }
+        }
+        return keys.keys;
+    }
+
+    // get all of the values specified for key in entries
+    string[] get_values(UrchinFSEntry[] entries, string key) {
+        bool[string] values;
+        foreach(entry; entries) {
+            string[]* key_values_ptr = (key in entry.metadata);
+            if(key_values_ptr !is null) {
+                string[] key_values = *key_values_ptr;
+                foreach(val; key_values) {
+                    values[val] = true;
+                }
+            }
+        }
+        return values.keys;
+    }
+
+    // get all of the display names for the given entries
     string[] get_listing(UrchinFSEntry[] entries) {
-        string[] result = [];
+        string[] result;
         foreach(entry; entries) {
             result ~= entry.display_name;
         }
         return result;
     }
 
+    // fitler the entries which have the given key
+    UrchinFSEntry[] filter(UrchinFSEntry[] entries, string key) {
+        UrchinFSEntry[] result;
+        foreach(i, entry; entries) {
+            string[]* key_values_ptr = (key in entry.metadata);
+            if(key_values_ptr !is null) {
+                result ~= entry;
+            }
+        }
+        return result;
+    }
+
+    // filter the entries by the given key and values
+    // if an entry has the key and has one of the values, it is retained
     UrchinFSEntry[] filter(UrchinFSEntry[] entries, string key, string[] values) {
-        UrchinFSEntry[] result = [];
+        UrchinFSEntry[] result;
         foreach(i, entry; entries) {
             bool keep = false;
 
@@ -98,6 +137,21 @@ class UrchinFS : Operations {
         return result;
     }
 
+    // set difference
+    string[] setdiff(string[] set, string[] minus) {
+        // clear way to do this
+        bool[string] map;
+        foreach(val; set) {
+            map[val] = true;
+        }
+        foreach(val; minus) {
+            map.remove(val);
+        }
+        return map.keys;
+    }
+
+    // for a split path parts, find the entries matching the specified key-value combinations
+    // and return the appropriate directory listing
     string[] get_results(const(char)[][] parts) {
         // strip off any empty leading sections of the path
         int cur = 0;
@@ -107,7 +161,11 @@ class UrchinFS : Operations {
         parts = parts[cur..parts.length];
 
         int index = 0;
-        string last_key = null;
+        string current_key = null;          // the currently selected key
+        string[] current_key_values = [];   // all of the potential values for this key
+        string[] current_values = [];       // the currently selected values
+
+
         string[][string] state;
         parsed last = parsed.NONE;
 
@@ -121,32 +179,34 @@ class UrchinFS : Operations {
             if(startsWith(part, FACET_PREFIX) && part != OR) {
                 // this is a key
                 last = parsed.KEY;
-                last_key = part;
+                string key = part[FACET_PREFIX.length .. $];
 
                 // fail on duplicate keys
-                string[]* key_values = (part in state);
+                string[]* key_values = (key in state);
                 if(key_values !is null) {
-                    stderr.writefln("Duplicate key [%s]", part);
+                    stderr.writefln("Duplicate key [%s]", key);
                     throw new FuseException(errno.ENOENT);
                 }
 
-                state[part] = [];
-                stdout.writefln("key: %s", part);
+                state[key] = [];
+                stdout.writefln("key: %s", key);
+                found = filter(found, key);
+                current_key_values = get_values(found, key);
 
                 if(is_last) {
                     // return the currrent valid values for this key
+                    stdout.writefln("key ret: %s", current_key_values);
+                    return current_key_values;
                 }
+                current_key = key;
+                current_values = [];
             } else if (last == parsed.VAL && part == OR) {
                 // this is an or
                 last = parsed.OR;
                 stdout.writefln("OR: %s", part);
                 
-                //setUnion()
-                //setIntersection()
-                //setDifference()
-
                 if(is_last) {
-                    // return the unused values for this key
+                   return setdiff(current_key_values, current_values);
                 }
 
             } else if(last == parsed.KEY || last == parsed.OR) {
@@ -154,18 +214,33 @@ class UrchinFS : Operations {
                 last = parsed.VAL;
                 stdout.writefln("val: %s", part);
 
-                // append a new array to values, containing the current part
-                state[last_key] = state[last_key] ~ part;
+                // this value either does not exist at all
+                // or is already selected, so throw an error
+                bool not_found = !current_key_values.canFind(part);
+                bool already_used = current_values.canFind(part);
+                if(not_found || already_used) {
+                    stdout.writefln("current key values: %s", current_key_values);
+                    stdout.writefln("not found? %s already used? %s", not_found, already_used);
+                    throw new FuseException(errno.ENOENT);
+                }
 
+                current_values ~= part;
+
+                // append a new array to values, containing the current part
+                state[current_key] = state[current_key] ~ part;
+
+                // lookahead, and if the next token is not an OR
+                // filter the entries by the current facet.
                 if(is_last || (!is_last && to!string(parts[index+1]) != OR)) {
-                    // lookahead, and if the next token is not an OR
-                    // filter the entries by the current facet
-                    found = filter(found, last_key, state[last_key]);
+                    found = filter(found, current_key, state[current_key]);
                 } 
 
                 if(is_last) {
                     // return the unused keys, OR, and the matching dirs
-                    get_listing(found);
+                    string[] ret = get_listing(found);
+                    stdout.writefln("val ret: %s", current_key_values);
+                    // FIXME
+                    return ret ~ facet_prefix(setdiff(get_keys(found), state.keys)) ~ OR;
                 }
             } else {
                 // this must be a dir
@@ -174,76 +249,19 @@ class UrchinFS : Operations {
 
                 if(is_last) {
                     // this is a symlink... don't use this as a return val???
+                    string[] ret;
+                    ret ~= "FIXME";
+                    return ret;
                 }
             }
             index++;
             stdout.writefln("state: %-(%s -> %s%)", state);
         }
         stdout.writeln("----");
-        return [];
+        // FIXME need to return all potential keys, prefixed with FACET_PREFIX
+        // FIXME as well as all entries
+        return get_listing(found) ~ facet_prefix(get_keys(found)) ~ "+" ~ "^";
     }
-
-    /*
-    string[] get_results(const(char)[][] parts, UrchinFSResult result) {
-        // possible paths showing up here:
-        // parts.length     path
-        // 0                /
-        // 1                /key
-        // 2                /key/value
-        // 3                /key/value/key
-        // 3                /key/value1/+OR
-        // 4                /key/value1/+OR/value2
-        // TODO tests: path/with///////multiple/slashes/
-        // TODO tests: / <- how is root handled?
-        // TODO tests: /key/value/NONEXISTENTKEY/
-        // TODO tests: /key/value-with-no-results/key/ ???
-        // TODO tests: /dir <- not a key
-
-
-        if(parts.empty) {
-            // FIXME generate the result listing
-            return result;
-        }
-
-        bool has_value = parts.length > 1;
-        bool has_or = parts.length > 2 && parts[2] == OR;
-
-        string key = parts[0];
-        bool[string][string] vals = (parts in metadata_cache);
-        if(vals !is null) {
-            // this was a valid key
-
-
-
-            if(has_value) {
-                value = parts[1];
-                // there is a value given on the path
-                bool[string] dirs = (value in vals);
-                if(dirs !is null) {
-                    // the value existed in the metadata map
-                    // find the 
-
-
-                    if(parts.length == 2) {
-                        // this was the last component
-                    }
-
-                } else {
-                    if(parts.length == 2) {
-                        // the value did not exist and this is
-                        // the last part of the directory path
-                        return [];
-                    }
-                }
-            } else {
-                // FIXME filter out the impossible keys somehow
-                return vals.keys;
-            }
-        }
-        // this was not a valid key
-        throw new FuseException(errno.ENOENT);
-    }
-    */
 
     override void getattr(const(char)[] path, ref stat_t s) {
         string[] results = get_results(path.split("/"));
